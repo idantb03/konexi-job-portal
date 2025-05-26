a0612c4dc58677af2a0a744b16e38964a14e8e28
Idan Personal <idantb3@gmail.com>
2025-05-26 22:29:38 +0700

    fix all job button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -45,7 +45,7 @@ export default function DashboardPage() {
                 </div>
                 <div className="ml-6 flex space-x-8">
                   <Link 
-                    href="/jobs" 
+                    href="/" 
                     className="inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-500 hover:text-gray-700"
                   >
                     View All Jobs
