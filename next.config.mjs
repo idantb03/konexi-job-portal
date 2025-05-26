6eb12b1bf9cef01d82e127095970c637a9213165
Idan Personal <idantb3@gmail.com>
2025-05-26 18:13:46 +0700

    fix(env): declaration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
+  },
 };
 
 export default nextConfig; 
